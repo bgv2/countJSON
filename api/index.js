5fc6526c770ebf68a5fca5f65378fd90976af36d
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-08-28 11:45:46 -0400

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,23 @@
+module.exports = (req, res) => {
+    https.get(decodeURI(req.body.jsonfile), (resp) => {
+
+        let data = '';
+        let q;
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            q = JSON.parse(data);
+        });
+        let c = 0;
+        for (var key in q) {
+            c += q[key].length;
+        }
+        res.send(c);
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+}
